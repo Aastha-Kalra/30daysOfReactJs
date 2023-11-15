49e36d6e8b01d710a2aea8657d2af9fd6093e6dd
Aastha kalra <aasthakalra2004@gmail.com>
2023-11-15 14:21:34 +0530

    ColorPicker Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {React} from 'react'
 import SearchFilter from './components/SearchFilter'
 import ItemsPerPage from './components/ItemsPerPage'
+import ColorPicker from './components/ColorPicker'
+
 function App() {
 
 
   return (
   <div className='bg-black h-screen'>
   {/* <SearchFilter/> */}
-  <ItemsPerPage/>
+  {/* <ItemsPerPage/> */}
+  <ColorPicker/>
+
   
   </div>
   )

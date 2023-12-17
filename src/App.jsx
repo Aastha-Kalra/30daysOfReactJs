bbf9cf1569f2dcbfbab691269ece5d1cb6dd7641
Aastha kalra <aasthakalra2004@gmail.com>
2023-12-17 17:35:36 +0530

    Project InputSubmission

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import ItemsPerPage from "./components/ItemsPerPage";
 import ColorPicker from "./components/ColorPicker";
 import Responsive from "./components/Responsive";
 import BharatClock from "./components/BhartClock";
-import Meal from "./components/Meal/Meal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RecipeInfo from "./components/Meal/RecipeInfo";
 import SocialButtons from "./components/SocialButtons";
 import LetterCalculator from "./components/LetterCalculator";
 import RandomNumber from "./components/RandomNumber";
@@ -26,12 +24,6 @@ function App() {
       {/* <ColorPicker/> */}
       {/* <Responsive/> */}
       {/* <BharatClock/> */}
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Meal />} />
-          <Route path="/:MealId" element={<RecipeInfo />} />
-        </Routes>
-      </BrowserRouter> */}
       {/* <SocialButtons/> */}
       {/* <LetterCalculator/> */}
       {/* <RandomNumber/> */}

6ddb0c4cfe29ae368f2081ac77764b58bdd5da15
Aastha kalra <aasthakalra2004@gmail.com>
2023-12-19 15:53:15 +0530

    added box

---
+++ src/DSA/TicTacToe/Box.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Box = ({value, onClick}) => {
+    const style = value === "X"? "text-yellow-500":"text-sky-400"
+  return (
+    <button className={`${style} bg-green-800 w-40 h-40 font-bold text-6xl rounded-xl text-white `} onClick={onClick}>{value}</button>
+  )
+}
+
+export default Box;

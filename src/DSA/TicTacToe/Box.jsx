ca46723635538c3dd11f4a091d0d4de82df7f15b
Aastha kalra <aasthakalra2004@gmail.com>
2023-12-20 21:43:30 +0530

    update boxes

--- src/DSA/TicTacToe/Box.jsx
+++ src/DSA/TicTacToe/Box.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = ({value, onClick}) => {
     const style = value === "X"? "text-yellow-500":"text-sky-400"
   return (
-    <button className={`${style} bg-green-800 w-40 h-40 font-bold text-6xl rounded-xl text-white `} onClick={onClick}>{value}</button>
+    <button className={`${style} bg-green-800 w-32 h-32 font-bold text-6xl rounded-xl text-white `} onClick={onClick}>{value}</button>
   )
 }
 

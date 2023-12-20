7a088c0cd39e48d9a3c55f8369ca2ba7e8d0836c
Aastha kalra <aasthakalra2004@gmail.com>
2023-12-20 18:49:19 +0530

    ADDING WIN CONDITIONS

--- src/DSA/TicTacToe/TicTacToe.jsx
+++ src/DSA/TicTacToe/TicTacToe.jsx
@@ -2,6 +2,20 @@ import React, { useState } from "react";
 import Board from "./Board";
 
 const TicTacToe = () => {
+
+  const Win_CONDITIONS = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8]   
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+  ]
+
+
+
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xplaying, setXplaying] = useState(true);
 
@@ -16,6 +30,7 @@ const TicTacToe = () => {
     setBoard(updatedBoard);
     setXplaying(!xplaying);
   };
+
 
   return (
     <div className="text-white px-44 flex flex-col justify-center items-center">

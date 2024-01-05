d360f6c06ddad727c47795728637080ee2906602
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-05 17:36:50 +0530

    Add&Sort function

--- src/DSA/DsaPractice.jsx
+++ src/DSA/DsaPractice.jsx
@@ -541,6 +541,23 @@ const DsaPractice = () => {
 // console.log(Count("Aastha Kalra"));
 
 
+// 49: Add and sort 
+//function addAndSort(array, element) {
+// 	array.push(element)
+// 	return array.sort((a,b)=>a-b)
+// }
+
+// // Sample Test Cases
+// console.log(addAndSort([5, 7, 1, 6], 3)); // Expected Output: [1, 3, 5, 6, 7]
+// console.log(addAndSort([15, 7, 2, 26], 10)); // Expected Output: [2, 7, 10, 15, 26]
+// console.log(addAndSort([25, 27, 21, 16], 19)); // Expected Output: [16, 19, 21, 25, 27]
+
+
+
+
+
+
+
 
   return (
     <div>

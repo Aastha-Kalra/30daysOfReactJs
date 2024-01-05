32c22d618df8b7bf06268f00e4feca4af05aa362
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-05 17:50:59 +0530

    getMiddleValue function

--- src/DSA/DsaPractice.jsx
+++ src/DSA/DsaPractice.jsx
@@ -554,6 +554,26 @@ const DsaPractice = () => {
 
 
 
+// 50: get middle value
+//  function getMiddleValue(arr) {
+// 	arr.sort((a, b) => a - b);
+
+// 	if (arr.length % 2 === 0) {
+// 		const mid1 = arr[arr.length / 2 - 1];
+// 		const mid2 = arr[arr.length / 2];
+// 		return (mid1 + mid2) / 2;
+// 	} else {
+// 		return arr[Math.floor(arr.length / 2)];
+// 	}
+// }
+
+// // Sample Test Cases
+// const numbers1 = [5, 3, 8, 4, 2];
+// const numbers2 = [40, 20, 60, 80, 50, 30];
+
+// console.log(getMiddleValue(numbers1)); // Expected Output: 4
+// console.log(getMiddleValue(numbers2)); 
+
 
 
 

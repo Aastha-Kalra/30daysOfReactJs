9e508d7ac171b20c4ffbe885214b084f3503b45b
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-06 19:51:51 +0530

    selection sort

--- src/DSA/Sorting.jsx
+++ src/DSA/Sorting.jsx
@@ -1,24 +1,52 @@
 // 1: bubble sort
-const BubbleSort = (arr) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i + 1];
-        arr[i + 1] = arr[i];
-        arr[i] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  return arr;
-};
+// const BubbleSort = (arr) => {
+//   let swapped;
+//   do {
+//     swapped = false;
+//     for (let i = 0; i < arr.length - 1; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         let temp = arr[i + 1];
+//         arr[i + 1] = arr[i];
+//         arr[i] = temp;
+//         swapped = true;
+//       }
+//     }
+//   } while (swapped);
+//   return arr;
+// };
 
-console.log("bubble sort ", BubbleSort([3, 12, 14, 1, 5, 8, 6]));
+// console.log("bubble sort ", BubbleSort([3, 12, 14, 1, 5, 8, 6]));
+
+// selection sort 
+const SelectionSort=(arr)=>{
+ for(let i=0;i<arr.length-1;i++){
+  let minIndex = i;
+  for(let j=i+1;j<arr.length;j++){
+    if(arr[minIndex]>arr[j]){
+      minIndex = j
+    }
+  }
+  if(minIndex !== i){
+    [arr[minIndex],arr[i]]= [arr[i],arr[minIndex]]
+  }
+ }
+ return arr
+}
+
+console.log("Selection sort ",SelectionSort([3,45,2,12,54,5]));
+
+
+
+
+
+
+
+
+
+
 
 const Sorting = () => {
-  return <div>Sorting</div>;
+  return <div className="text-white text-9xl ">Sorting</div>;
 };
 
 export default Sorting;

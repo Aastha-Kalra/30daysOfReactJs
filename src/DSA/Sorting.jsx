f1797f29f33066c440acd5234d8a3ef3a04ea1c1
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-07 20:41:05 +0530

    insertion sort

--- src/DSA/Sorting.jsx
+++ src/DSA/Sorting.jsx
@@ -18,26 +18,39 @@
 // console.log("bubble sort ", BubbleSort([3, 12, 14, 1, 5, 8, 6]));
 
 // selection sort 
-const SelectionSort=(arr)=>{
- for(let i=0;i<arr.length-1;i++){
-  let minIndex = i;
-  for(let j=i+1;j<arr.length;j++){
-    if(arr[minIndex]>arr[j]){
-      minIndex = j
+// const SelectionSort=(arr)=>{
+//  for(let i=0;i<arr.length-1;i++){
+//   let minIndex = i;
+//   for(let j=i+1;j<arr.length;j++){
+//     if(arr[minIndex]>arr[j]){
+//       minIndex = j
+//     }
+//   }
+//   if(minIndex !== i){
+//     [arr[minIndex],arr[i]]= [arr[i],arr[minIndex]]
+//   }
+//  }
+//  return arr
+// }
+
+// console.log("Selection sort ",SelectionSort([3,45,2,12,54,5]));
+
+
+// Insertion Sort 
+const InsertionSort = (arr)=>{
+  for(let i=1;i<arr.length;i++){
+    let current = arr[i]
+    let j = i-1;
+    while(j>=0 && arr[j]> current){
+      arr[j+1]= arr[j];
+      j--;
     }
+    arr[j+1]= current
+    
   }
-  if(minIndex !== i){
-    [arr[minIndex],arr[i]]= [arr[i],arr[minIndex]]
-  }
- }
- return arr
+  return arr;
 }
-
-console.log("Selection sort ",SelectionSort([3,45,2,12,54,5]));
-
-
-
-
+console.log("Insertion sort",InsertionSort([12,23,41,65,67,4,2]));
 
 
 

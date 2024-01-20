f0afd8c8601d150bc5126cea925a0203d1b6d8bc
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-20 17:33:12 +0530

    added header

---
+++ src/React-Assessments/Emoji/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-green-500 font-bold text-2xl w-full flex justify-center items-center p-4'>
+    Search your Emoji's According to your Mood...
+    </div>
+  )
+}
+
+export default Header

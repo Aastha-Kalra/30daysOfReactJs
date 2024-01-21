3980db338b8707a4a73ed1fa15b90c811c38d8dc
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-21 17:17:07 +0530

    copyToClipboard with selected part

--- src/React-Assessments/Emoji/SearchResult.jsx
+++ src/React-Assessments/Emoji/SearchResult.jsx
@@ -1,31 +1,37 @@
-import React, { useCallback, useRef } from "react";
+import React, { useState } from "react";
 
 const SearchResult = ({ filtered }) => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const copyToClipboard = (src) => {
+    navigator.clipboard.writeText(src);
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    copyToClipboard(item);
+  };
+
   return (
     <div className="grid grid-cols-3 gap-10 px-44">
       {filtered.map((item) => {
         const HexCode = item.symbol.codePointAt(0).toString(16);
         const src = `//cdn.jsdelivr.net/emojione/assets/png/${HexCode}.png`;
 
-        const passwordRef = useRef();
-        const copytoClipboard = useCallback(() => {
-          passwordRef.current?.select();
-          window.navigator.clipboard.writeText(src);
-        }, [src]);
-
         return (
           <div
             className="bg-white/5 p-14 text-center items-center flex flex-col text-2xl gap-3 rounded"
             key={item.title}
           >
-            <label
-              htmlFor=""
-              className="cursor-pointer"
-              onClick={copytoClipboard}
-            >
-              copy
-            </label>
-            <img src={src} alt={item.title} />
+            <img
+              src={src}
+              alt={item.title}
+              onClick={() => handleItemClick(src)}
+              className={` ${
+                selectedItem === src
+                  ? "border-2 border-blue-500 rounded p-2 "
+                  : ""
+              } cursor-pointer`}
+            />
             <p>{item.title}</p>
           </div>
         );

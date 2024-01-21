4afabf50c594620ac892a7b62a8566dbc1286fcb
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-21 16:47:15 +0530

    EmojiSearch file

--- src/React-Assessments/Emoji/EmojiSearch.jsx
+++ src/React-Assessments/Emoji/EmojiSearch.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "./Header";
+import InputSearch from "./InputSearch";
+import SearchEmoji from "./Searchemoji";
+import SearchResult from "./SearchResult";
 
 const EmojiSearch = () => {
+  const [filtered, setFiltered] = useState(SearchEmoji("", 50));
+  const handleSearch = (e) => {
+    const filter = SearchEmoji(e.target.value);
+    setFiltered(filter);
+  };
   return (
-    <div>EmojiSearch</div>
-  )
-}
+    <div>
+      <Header />
+      <InputSearch handleSearch={handleSearch} />
+      <SearchResult filtered={filtered} />
+    </div>
+  );
+};
 
-export default EmojiSearch
+export default EmojiSearch;

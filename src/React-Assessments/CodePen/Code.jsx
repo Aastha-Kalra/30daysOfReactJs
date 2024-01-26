ffd898ecc5f1c61622043aa97b09585ed01b62e3
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-26 18:20:46 +0530

    codepen clone

---
+++ src/React-Assessments/CodePen/Code.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Editor from './Editor'
+
+const Code = () => {
+  return (
+    <div>
+        <Editor/>
+        <Editor/>
+        <Editor/>
+    </div>
+  )
+}
+
+export default Code

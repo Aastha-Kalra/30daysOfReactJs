d454ef67f2617f2385f5d6b288d9557837f64b17
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-29 21:46:15 +0530

    MaxCount practice updated

--- src/React-Assessments/MaxCount/MaxCount.jsx
+++ src/React-Assessments/MaxCount/MaxCount.jsx
@@ -1,16 +1,52 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 const MaxCount = () => {
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(10);
+  const [intervalId, setIntervalId] = useState(null);
+  const handleCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prev) => (prev > 0 ? prev - 1 : 0));
+    }, 1000);
+
+    setIntervalId(interval);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  useEffect(() => {
+    if (time === 0) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }, [time, intervalId]);
+
   return (
-    <div>
+    <div className="flex flex-col justify-center p-4 items-center gap-4">
       <div>MaxCount</div>
       <div>
-        Implement a button and timmer that will start from 10 and ends at 0 and
-        tracks the counter untill the timer expires and button should disapper
-        once timer expires
+        Implement a button and timer that will start from 10 and ends at 0 and
+        tracks the counter until the timer expires, and the button should
+        disappear once the timer expires.
       </div>
 
-      <div>No. of clicks until timer expires</div>
+      <div className="text-5xl">No. of clicks until timer expires: {count}</div>
+      <div className="text-4xl">Time left: {time}</div>
+      {time > 0 && (
+        <div>
+          <button
+            onClick={handleCount}
+            className="bg-sky-500 px-4 py-2 text-white my-4"
+          >
+            Increment
+          </button>
+        </div>
+      )}
     </div>
   );
 };

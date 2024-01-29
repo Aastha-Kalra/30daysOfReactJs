0d0529de9485ab3873d4b3772461e4fdfd6817f9
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-29 21:21:14 +0530

    MaxCount practice

---
+++ src/React-Assessments/MaxCount/MaxCount.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MaxCount = () => {
+  return (
+    <div>
+      <div>MaxCount</div>
+      <div>
+        Implement a button and timmer that will start from 10 and ends at 0 and
+        tracks the counter untill the timer expires and button should disapper
+        once timer expires
+      </div>
+
+      <div>No. of clicks until timer expires</div>
+    </div>
+  );
+};
+
+export default MaxCount;

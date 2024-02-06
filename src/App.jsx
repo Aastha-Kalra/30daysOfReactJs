824f1e0db7b2a10070c41da4d695a2aa549bc8cc
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-06 16:20:22 +0530

    added useLayout

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import Dictionary from "./components/Dictionary/Dictionary";
 import InputSubmission from "./components/InputSubmission";
 import TicTacToe from "./DSA/TicTacToe/TicTacToe";
 import Timer from "./components/Timer";
-import SimpleInterestCal from './components/SimpleInterestCal'
-import PollComponent from './components/VoteCount'
+import SimpleInterestCal from "./components/SimpleInterestCal";
+import PollComponent from "./components/VoteCount";
 import Calender from "./components/Cal";
 import Today from "./components/today";
 import CharacterCount from "./components/CharacterCount";
@@ -34,16 +34,16 @@ import DaysUntilElement from "./components/DaysUntilElement";
 import Local from "./components/LocalStorage";
 import Sorting from "./DSA/Sorting";
 // import updateTemplateLiteral from './components/TemplateLiterals';
-import Question1 from './React-Assessments/Question1/Question1'
-import Question2 from './React-Assessments/Question2/Question2'
-import Question3 from './React-Assessments/Question3/Question3'
+import Question1 from "./React-Assessments/Question1/Question1";
+import Question2 from "./React-Assessments/Question2/Question2";
+import Question3 from "./React-Assessments/Question3/Question3";
 // import Counter from "./components/ReactCounterBug";
 // import Toggle from './components/Toggle'
 // import TemperatureConverter from "./components/TemperatureCon";
 // import MessageFeed from "./components/SubmitMessages";
 // import SignUpForm from "./components/ValidateSignUp";
 // import CVCreator from "./components/CVCreator";
-import SearchFilter from "./React-Assessments/Question4/SearchFilter"; 
+import SearchFilter from "./React-Assessments/Question4/SearchFilter";
 import ImageSlider from "./React-Assessments/Question5/ImageSlider";
 import EmojiSearch from "./React-Assessments/Emoji/EmojiSearch";
 import Joke from "./React-Assessments/Joke/Joke";
@@ -59,6 +59,11 @@ import UseWindowDimensions from "./React-Assessments/CustomHooks/UseWindowDimens
 import UseFetch from "./React-Assessments/CustomHooks/UseFetch";
 import Main from "./React-Assessments/LazyLoading/Main";
 import ProgressBar from "./React-Assessments/ProgressBar/ProgressBar";
+import Hook from "./React-Assessments/UseContextHook.jsx/Hook";
+import Register from "./React-Assessments/UseContextHook.jsx/Register";
+import Login from "./React-Assessments/UseContextHook.jsx/Login";
+import UseReducer from "./React-Assessments/UseReducerHook.jsx/UseReducer";
+import UseLayout from "./React-Assessments/UseLayout";
 function App() {
   return (
     <div className="bg-black min-h-screen w-full overflow-hidden text-green-500 p-4">
@@ -71,56 +76,67 @@ function App() {
       {/* <LetterCalculator/> */}
       {/* <RandomNumber/> */}
       {/* <CountryCapital/> */}
-   {/* <DsaPractice/> */}
-   {/* <Pexels/> */}
-   {/* <NewYear/> */}
-  {/* <QRcode/> */}
-  {/* <Dictionary/> */}
-  {/* <InputSubmission/> */}
-  {/* <TicTacToe/> */}
-  {/* <Timer/> */}
-{/* <SimpleInterestCal/> */}
-{/* <PollComponent/> */}
-{/* <Calender/> */}
-{/* <Today/> */}
-{/* <CharacterCount/> */}
-{/* <Rating/> */}
-{/* <Tip/> */}
-{/* <DistanceConverter/> */}
-{/* <Tabs/> */}
-{/* <Table/> */}
-{/* <RandomPalette/> */}
-{/* <Calculator/>
+      {/* <DsaPractice/> */}
+      {/* <Pexels/> */}
+      {/* <NewYear/> */}
+      {/* <QRcode/> */}
+      {/* <Dictionary/> */}
+      {/* <InputSubmission/> */}
+      {/* <TicTacToe/> */}
+      {/* <Timer/> */}
+      {/* <SimpleInterestCal/> */}
+      {/* <PollComponent/> */}
+      {/* <Calender/> */}
+      {/* <Today/> */}
+      {/* <CharacterCount/> */}
+      {/* <Rating/> */}
+      {/* <Tip/> */}
+      {/* <DistanceConverter/> */}
+      {/* <Tabs/> */}
+      {/* <Table/> */}
+      {/* <RandomPalette/> */}
+      {/* <Calculator/>
 <DaysUntilElement/> */}
-{/* <Local/> */}
-{/* <Recursion/> */}
-{/* <updateTemplateLiteral/> */}
-{/* <Sorting/> */}
-{/* <Question1/> */}
-{/* <Question2/> */}
-{/* <Question3/> */}
-{/* <Counter/> */}
-{/* <Toggle/> */}
-{/* <TemperatureConverter/> */}
-{/* <MessageFeed/> */}
-{/* <SignUpForm/> */}
-{/* <CVCreator/> */}
-{/* <SearchFilter/> */}
-{/* <ImageSlider/> */}
-{/* <EmojiSearch/> */}
-{/* <Joke/> */}
-{/* <TextEditorApp/> */}
-{/* <MaxCount/> */}
-{/* <RemoveSpecificButton/> */}
-{/* <UnnecessaryFunctionCall/> */}
-{/* <CalculateFactorial/> */}
-{/* <CountInputChanges/> */}
-{/* <Theme/> */}
-{/* <PageBottom/> */}
-{/* <UseWindowDimensions/> */}
-{/* <UseFetch/> */}
-{/* <Main/> */}
-<ProgressBar/>
+      {/* <Local/> */}
+      {/* <Recursion/> */}
+      {/* <updateTemplateLiteral/> */}
+      {/* <Sorting/> */}
+      {/* <Question1/> */}
+      {/* <Question2/> */}
+      {/* <Question3/> */}
+      {/* <Counter/> */}
+      {/* <Toggle/> */}
+      {/* <TemperatureConverter/> */}
+      {/* <MessageFeed/> */}
+      {/* <SignUpForm/> */}
+      {/* <CVCreator/> */}
+      {/* <SearchFilter/> */}
+      {/* <ImageSlider/> */}
+      {/* <EmojiSearch/> */}
+      {/* <Joke/> */}
+      {/* <TextEditorApp/> */}
+      {/* <MaxCount/> */}
+      {/* <RemoveSpecificButton/> */}
+      {/* <UnnecessaryFunctionCall/> */}
+      {/* <CalculateFactorial/> */}
+      {/* <CountInputChanges/> */}
+      {/* <Theme/> */}
+      {/* <PageBottom/> */}
+      {/* <UseWindowDimensions/> */}
+      {/* <UseFetch/> */}
+      {/* <Main/> */}
+      {/* <ProgressBar/> */}
+
+      {/* <div className="flex flex-col justify-center items-center text-3xl gap-3">
+        <Hook />
+        <div className="flex justify-center gap-4">
+          <Register />
+          <Login />
+        </div>
+      </div> */}
+
+      {/* <UseReducer/> */}
+      <UseLayout/>
     </div>
   );
 }

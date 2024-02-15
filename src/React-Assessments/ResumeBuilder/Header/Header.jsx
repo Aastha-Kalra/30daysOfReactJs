92d1e62f95f658f3b8c7dc798a6823802da5a906
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-15 18:04:39 +0530

    updated header

--- src/React-Assessments/ResumeBuilder/Header/Header.jsx
+++ src/React-Assessments/ResumeBuilder/Header/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='text-4xl flex justify-center items-center px-3 my-5'>
-        Create a Resume that makes you stand out! 
+    <div className="text-4xl flex justify-center flex-col gap-4 items-center px-3 my-5">
+      <p> Resume Builder </p>
+      Create a Resume that makes you stand out!
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

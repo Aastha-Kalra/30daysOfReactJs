de59a5680a949d03fb37805af76b3b47ca3b5f65
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-15 17:55:48 +0530

    added header of resume

---
+++ src/React-Assessments/ResumeBuilder/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-4xl flex justify-center items-center px-3 my-5'>
+        Create a Resume that makes you stand out! 
+    </div>
+  )
+}
+
+export default Header

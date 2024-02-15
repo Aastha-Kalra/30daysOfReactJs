96cd99498271bfe406cc87599ce79a2c83ceeeb3
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-15 17:55:20 +0530

    ResumeBuikder app

---
+++ src/React-Assessments/ResumeBuilder/ResumeBuild.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header/Header'
+import Body from './Body/Body'
+
+const ResumeBuild = () => {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+    </div>
+  )
+}
+
+export default ResumeBuild

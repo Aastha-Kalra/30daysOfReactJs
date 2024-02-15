7c27155a1ad428924772a210600de74e5d1e0c92
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-15 17:55:54 +0530

    added body of resume

---
+++ src/React-Assessments/ResumeBuilder/Body/Body.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Body = () => {
+  const colors = [
+    "#2421b8",
+    "#cdd61e",
+    "#d6271e",
+    "#d61e9f",
+    "#262626",
+    "#136b0e",
+  ];
+  return (
+    <div className="flex justify-between items-center px-6 ">
+      <div className="flex justify-start my-10 items-center gap-4">
+        {colors.map((item) => {
+          return (
+            <div>
+              <div
+                key={item}
+                style={{ background: item }}
+                className="w-14 h-14 rounded-full cursor-pointer transform ease-in duration-300   scale-90 hover:scale-110"
+              ></div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <button className="bg-sky-600 px-7 py-3 text-white text-lg rounded-md">
+          Download
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Body;

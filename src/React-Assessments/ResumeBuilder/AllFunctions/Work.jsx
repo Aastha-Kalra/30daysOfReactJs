f116233d752657b38b4de18820879e71c2c9db92
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-16 21:47:47 +0530

    work file

---
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InputControl from "../InputControl/InputControl";
+const Work = () => {
+  return (
+    <>
+      <div>
+        <InputControl label="work " placeholder="Enter your experience" />
+      </div>
+    </>
+  );
+};
+
+export default Work;

d75b5633dc8c33c4603843be29f82fb7a3ba404a
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-16 21:47:07 +0530

    my editor file

---
+++ src/React-Assessments/ResumeBuilder/Editor/Editor.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import BasicInfo from "../AllFunctions/BasicInfo";
+import Work from "../AllFunctions/Work";
+
+const Editor = ({ sections }) => {
+  const [activeSection, setActiveSection] = useState(Object.keys(sections)[0]);
+
+  const generateBody = () => {
+    switch (sections[activeSection]) {
+      case sections.basicInfo:
+        return <BasicInfo />;
+      case sections.workExp:
+        return <Work />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="border-2 h-96 mx-44 rounded-md">
+      <div className="flex justify-center items-center gap-4 text-xl bg-white/10 py-3 shadow-slate-400 ">
+        {Object.keys(sections)?.map((key) => (
+          <div
+            className={`py-2 cursor-pointer ${
+              activeSection === key
+                ? "border-yellow-500 border-b-2 text-yellow-500"
+                : null
+            }`}
+            key={key}
+            onClick={() => setActiveSection(key)}
+          >
+            {sections[key]}
+          </div>
+        ))}
+      </div>
+      <div>{generateBody()}</div>
+    </div>
+  );
+};
+
+export default Editor;
+

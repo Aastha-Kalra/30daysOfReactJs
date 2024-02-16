0b8368b9d96ff441598390928101a9ee37a0bf42
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-16 21:47:41 +0530

    BasicInfo file

---
+++ src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import InputControl from '../InputControl/InputControl'
+
+const BasicInfo = () => {
+  return (
+    <div>
+    <InputControl label="Name" placeholder="Enter your Name"/>
+    <InputControl label="Name" placeholder="Enter your Title eg. Frontend Developer"/>
+    <InputControl label="Name" placeholder="Enter your Name"/>
+    <InputControl label="Name" placeholder="Enter your Name"/>
+    </div>
+  )
+}
+
+export default BasicInfo

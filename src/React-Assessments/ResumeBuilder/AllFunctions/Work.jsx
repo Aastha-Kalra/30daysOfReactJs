80b2bf64551d26330e7ddc9551e80f425dceaa18
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-17 21:14:31 +0530

    added work fields

--- src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
@@ -4,7 +4,18 @@ const Work = () => {
   return (
     <>
       <div>
-        <InputControl label="work " placeholder="Enter your experience" />
+        <InputControl type="number" label="Work " placeholder="Enter your experience (in years)" />
+        <InputControl  label="Title" placeholder="Enter your Title eg. Frontend Developer"/>
+        <InputControl label="Company Name" placeholder="Enter your company name eg. Microsoft"/>
+        <InputControl type="date" label="Start date"/>
+        <InputControl type="date" label="End date" />
+       <div className="">
+        <label className="text-xl pl-16">Description</label>
+       <InputControl placeholder="Write about your experience"/>
+       <InputControl placeholder="Write about your experience"/>
+       </div>
+        
+        
       </div>
     </>
   );

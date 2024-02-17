696f788db0a94c84774bcab61a9dc057dec69e58
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-17 21:24:02 +0530

    added more projects

--- src/React-Assessments/ResumeBuilder/AllFunctions/Projects.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Projects.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import InputControl from '../InputControl/InputControl'
+import React from "react";
+import InputControl from "../InputControl/InputControl";
 const Projects = () => {
   return (
     <div>
-<InputControl label="Title" placeholder=""/>
+      <InputControl
+        label="Title"
+        placeholder="Enter Project Title eg. Food App"
+      />
+      <InputControl
+        label="Title"
+        placeholder="Enter Project Title eg. Food App"
+      />
+      <InputControl
+        label="Deployed Link"
+        placeholder="Enter Live link of project"
+      />
+      <InputControl
+        label="Github Link"
+        placeholder="Enter Github link of project"
+      />
+      <InputControl label="Overview" placeholder="Line 1" />
+      <InputControl placeholder="Line 2" />
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;

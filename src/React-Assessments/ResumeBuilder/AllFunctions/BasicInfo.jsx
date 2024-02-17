e950725f37484550def06885415520cddbfb8f5a
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-17 21:14:43 +0530

    added basicInfo fields

--- src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
@@ -5,9 +5,9 @@ const BasicInfo = () => {
   return (
     <div>
     <InputControl label="Name" placeholder="Enter your Name"/>
-    <InputControl label="Name" placeholder="Enter your Title eg. Frontend Developer"/>
-    <InputControl label="Name" placeholder="Enter your Name"/>
-    <InputControl label="Name" placeholder="Enter your Name"/>
+    <InputControl label="Title" placeholder="Enter your Title eg. Frontend Developer"/>
+    <InputControl label="Linkedin Link" placeholder="Enter your Linkedin profile link "/>
+    <InputControl label="Github Link" placeholder="Enter your Github profile link "/>
     </div>
   )
 }

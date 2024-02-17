d68cbf711a9b1f3a9569d085c299aa8fb8068f2c
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-17 21:14:12 +0530

    updated InputControl file

--- src/React-Assessments/ResumeBuilder/InputControl/InputControl.jsx
+++ src/React-Assessments/ResumeBuilder/InputControl/InputControl.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const InputControl = ({ label, ...props }) => {
   return (
     <div className="flex gap-4 text-xl p-3 w-full px-14">
-      {label && <label className="p-3">{label}</label>}
-      <input className="focus:outline-none p-2 rounded-md w-full bg-transparent border-2 border-yellow-200" type="text" {...props} />
+      <div className="w-96">
+      {label && <label className="p-3 w-full">{label}</label>}
+      </div>
+     <div className="w-full">
+     <input className="focus:outline-none p-2 rounded-md w-full bg-transparent border-2 border-yellow-800 " type="text" {...props} />
+     </div>
     </div>
   );
 };

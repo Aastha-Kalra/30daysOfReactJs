035fdfadc02d745c7d6ceb396200d7e19cb9b937
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-17 21:15:23 +0530

    added project file

---
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import InputControl from '../InputControl/InputControl'
+const Projects = () => {
+  return (
+    <div>
+<InputControl label="Title" placeholder=""/>
+    </div>
+  )
+}
+
+export default Projects

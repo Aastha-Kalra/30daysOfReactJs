7377d777a098df7e3e460b8643ac068b8bfa05b3
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-18 20:38:23 +0530

    updated achievements

--- src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
@@ -3,10 +3,10 @@ import InputControl from '../InputControl/InputControl'
 const Achievement = () => {
   return (
     <div>
-        <label htmlFor="">List your Achievements</label>
+        <label htmlFor="" className='text-xl flex justify-center items-center my-3'>List your Achievements</label>
         <InputControl label="Title" placeholder = "Enter the title eg. Reactjs Developer Ceritificate"/>
         <InputControl label="Description" placeholder = "Line 1 "/>
-        <InputControl label="Description" placeholder = "Line 2 "/>
+        <InputControl placeholder = "Line 2 "/>
     </div>
   )
 }

d0543ea0b24a8904862bd819e6940de5c22ad64d
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-20 21:25:31 +0530

    Summary file

---
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Summary.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputControl from '../InputControl/InputControl'
+
+const Summary = () => {
+  return (
+    <div>
+        <InputControl label="Summary" placeholder="Enter your Summary/Objective"/>
+    </div>
+  )
+}
+
+export default Summary

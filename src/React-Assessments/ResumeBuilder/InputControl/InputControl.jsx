e1ffacacafd7ee38be237777b60603a503bc63fd
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-22 21:12:08 +0530

    update inputControl

--- src/React-Assessments/ResumeBuilder/InputControl/InputControl.jsx
+++ src/React-Assessments/ResumeBuilder/InputControl/InputControl.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const InputControl = ({ label, ...props }) => {
+const InputControl = ({ label,onchange,...props}) => {
   return (
-    <div className="flex gap-4 text-xl p-3 w-full px-14">
+    <div className="flex gap-4 text-xl py-4 w-full px-14">
       <div className="w-96">
-      {label && <label className="p-3 w-full">{label}</label>}
+      {label && <label className="p-3 w-full font-semibold text-2xl">{label}</label>}
       </div>
      <div className="w-full">
-     <input className="focus:outline-none p-2 rounded-md w-full bg-transparent border-2 border-yellow-800 " type="text" {...props} />
+     <input className="focus:outline-none p-2 rounded-md w-full bg-transparent border-2 border-yellow-800 " type="text" onChange={onchange} {...props} />
      </div>
     </div>
   );

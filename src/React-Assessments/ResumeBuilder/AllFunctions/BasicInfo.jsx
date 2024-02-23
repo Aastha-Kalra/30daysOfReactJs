1dac2f5f29cef385793ab0c6049cbedefadc6299
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-23 21:59:12 +0530

    added sections

--- src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
@@ -5,9 +5,11 @@ const BasicInfo = () => {
   return (
     <div>
     <InputControl label="Name" placeholder="Enter your Name"/>
-    <InputControl label="Title" placeholder="Enter your Title eg. Frontend Developer"/>
+    <InputControl label="Position" placeholder="Enter your Position eg. Frontend Developer"/>
     <InputControl label="Linkedin Link" placeholder="Enter your Linkedin profile link "/>
     <InputControl label="Github Link" placeholder="Enter your Github profile link "/>
+    <InputControl label="Phone Number" placeholder="Enter your Phone number "/>
+    <InputControl label="Email Id " placeholder="Enter your Email Id "/>
     </div>
   )
 }

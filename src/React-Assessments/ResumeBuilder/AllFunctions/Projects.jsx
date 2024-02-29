15050026d877b8aa9d9339ca317fbdf614f8e300
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-29 22:01:08 +0530

    accessing values to projects

--- src/React-Assessments/ResumeBuilder/AllFunctions/Projects.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Projects.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import InputControl from "../InputControl/InputControl";
-const Projects = () => {
+const Projects = ({ value }) => {
   return (
     <div>
       <InputControl
         label="Title"
         placeholder="Enter Project Title eg. Food App"
+        defaultValue={value.projectName}
       />
       <InputControl
         label="Deployed Link"
         placeholder="Enter Live link of project"
+        defaultValue={value.projectName}
       />
       <InputControl
         label="Github Link"
         placeholder="Enter Github link of project"
+        defaultValue={value.projectName}
       />
-      <InputControl label="Overview" placeholder="Line 1" />
-      <InputControl placeholder="Line 2" />
+      <InputControl
+        label="Overview"
+        placeholder="Line 1"
+        defaultValue={value.projectName}
+      />
+      <InputControl placeholder="Line 2" defaultValue={value.projectName} />
     </div>
   );
 };

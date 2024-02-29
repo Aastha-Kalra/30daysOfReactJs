d156a2e536a2306d2a435c39d2070f361072636b
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-29 22:01:30 +0530

    added values to work

--- src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
@@ -1,21 +1,48 @@
 import React from "react";
 import InputControl from "../InputControl/InputControl";
-const Work = () => {
+const Work = ({ value }) => {
   return (
     <>
       <div>
-        <InputControl type="number" label="Work " placeholder="Enter your experience (in years)" />
-        <InputControl  label="Position" placeholder="Enter your Position eg. Frontend Developer"/>
-        <InputControl label="Company Name" placeholder="Enter your company name eg. Microsoft"/>
-        <InputControl type="date" label="Start date"/>
-        <InputControl type="date" label="End date" />
-       <div>
-        <label className="text-2xl font-semibold pl-[68px] ">Description</label>
-       <InputControl placeholder="Write about your experience"/>
-       <InputControl placeholder="Write about your experience"/>
-       </div>
-        
-        
+        <InputControl
+          type="number"
+          label="Work "
+          placeholder="Enter your experience (in years)"
+          defaultValue={value.experience}
+        />
+        <InputControl
+          label="Position"
+          placeholder="Enter your Position eg. Frontend Developer"
+          defaultValue={value.position}
+        />
+        <InputControl
+          label="Company Name"
+          placeholder="Enter your company name eg. Microsoft"
+          defaultValue={value.companyName}
+        />
+        <InputControl
+          type="date"
+          label="Start date"
+          defaultValue={value.startDate}
+        />
+        <InputControl
+          type="date"
+          label="End date"
+          defaultValue={value.endDate}
+        />
+        <div>
+          <label className="text-2xl font-semibold pl-[68px] ">
+            Description
+          </label>
+          <InputControl
+            placeholder="Write about your experience"
+            defaultValue={value.points ? value.points[0] : ""}
+          />
+          <InputControl
+            placeholder="Write about your experience"
+            defaultValue={value.points ? value.points[1] : ""}
+          />
+        </div>
       </div>
     </>
   );

73e2af13a2ff85f0090344b3a3fb2ccf06652c62
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-01 22:08:51 +0530

    React19 features

---
+++ src/React19/React19.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const React19 = () => {
+  return (
+    
+    <div className='flex flex-col justify-center items-center text-3xl py-5'>
+    
+    </div>
+  )
+}
+
+export default React19

70fc992be4c7f0f008162d9475f341d5694eb8ae
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-02 18:04:37 +0530

    use Server Use client feature

---
+++ src/React19/UseServerUseClient.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UseServerUseClient = () => {
+  return (
+    <div id='use'>
+      Talking about the actual client versus server directives so you know that
+      you can put use client or use server at the top of a file inside of nextjs
+      to determine if that code is running on the server or if that code is
+      being pushed down and ran on the client so this is just a really easy way
+      for you to distinguish between these two things and right now it's really
+      only usable inside of nextjs but with this newest version of react 19
+      that's coming out this is going to be a stable release instead of
+      something that is more of an experimental status which means other
+      Frameworks can be built around these used client and used server features
+      and we will hopefully see more Frameworks than just nextjs or remix
+      actually have these used client and used server features and we can really
+      start to play around with the power of this in more than just nextjs which
+      I think is really cool and important because more and more Frameworks add
+      to more Innovation which pushes all the Frameworks further. 
+    </div>
+  );
+};
+
+export default UseServerUseClient;

c4fd25f4dcbc351138574be3d160d08a2d47fd63
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-02 18:03:54 +0530

    added navbar and actions

--- src/React19/React19.jsx
+++ src/React19/React19.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Compiler from "./Compiler";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Actions from "./Actions";
+import Nav from "./Nav";
 
 const React19 = () => {
   return (
     <div>
       <BrowserRouter>
+      <Nav/>
         <Routes>
           <Route element={<Compiler />} path="/" />
+          <Route element={<Actions />} path="/actions" />
         </Routes>
       </BrowserRouter>
     </div>

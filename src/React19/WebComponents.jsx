7ce917bc6a456ccd81ea3ab725d6a51b32aeb52c
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-03 20:35:18 +0530

    webComponents feature

---
+++ src/React19/WebComponents.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WebComponents = () => {
+  return (
+    <div id="web">
+      web components are going to work much better in react 19 they mention it
+      very briefly inside this article it's like one sentence that they mention
+      it but they do mention that web components are now going to be working
+      much better with react 19 so if you want to use web components in react it
+      should be much easier to implement them inside of a react project because
+      in the past that was something that was really difficult which is why you
+      would almost always see when you're working at libraries there would be a
+      web component library and then like a react specific version because it
+      was so hard to make web components work in react but now it should be much
+      easier to get them to play well together
+    </div>
+  );
+};
+
+export default WebComponents;

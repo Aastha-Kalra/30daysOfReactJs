ab0ecb5d27c0a73f08e8d08955c887e25e093d15
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-05 18:10:22 +0530

    useHook feature

---
+++ src/React19/UseHook.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const UseHook = () => {
+  return (
+    <div>
+      you actually won't really need to use react.lazy anymore and that's because
+      a brand new hook called use just simply use is being added to react and
+      this use hook allows you to do a lot of really cool stuff it allows you to
+      essentially asynchronously load things whether that's going to be
+      asynchronously loading JavaScript files whether it's going to be
+      asynchronously running different proc promises or maybe you even want to
+      run your context through here all of those things are possible with the
+      use hook it's honestly a rather complex hook that breaks all of the rules
+      of normal hooks
+    </div>
+  );
+};
+
+export default UseHook;

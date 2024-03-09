0d146f45ece785aa5a335d09bc21bb1b7088b8a8
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-09 18:14:52 +0530

    roomReservation

---
+++ src/RoomReservation/RoomBooking.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Room from "./Room";
+import Sidebar from "./Sidebar";
+import { USER_DATA } from "./utils";
+import { ROOMS_DATA } from "./utils";
+const RoomBooking = () => {
+  const [user, setUser] = useState(USER_DATA);
+  const [bookedRooms, setBookedRooms] = useState([]);
+  const [roomData, setRoomData] = useState(ROOMS_DATA);
+  const handleRoomClick = (room) => {
+    if (!room.isReserved && room.cost <= user.credits) {
+      setUser((prevUser) => ({
+        ...prevUser,
+        credits: prevUser.credits - room.cost,
+      }));
+      setBookedRooms((prevRooms) => [...prevRooms, room]);
+      setRoomData((prevRooms) =>
+        prevRooms.map((item) =>
+          item.id === room.id ? { ...item, isReserved: true } : item
+        )
+      );
+    }
+  };
+
+  return (
+    <div className="flex mx-9 my-5 w-full">
+      <div className="grid grid-cols-3 gap-6 w-full ">
+        {roomData.map((room) => {
+          return (
+            <div>
+              <Room
+                room={room}
+                credits={user.credits}
+                onroomClick={handleRoomClick}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <Sidebar
+        bookedRooms={bookedRooms}
+        user={user}
+        availableRooms={availableRooms}
+      />
+    </div>
+  );
+};
+
+export default RoomBooking;

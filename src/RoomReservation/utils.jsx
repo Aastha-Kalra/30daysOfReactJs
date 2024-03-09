22d0ac03d132a5541e312ce7d0ffee8504755db0
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-09 18:13:15 +0530

    updated utils

--- src/RoomReservation/utils.jsx
+++ src/RoomReservation/utils.jsx
@@ -1,22 +1,12 @@
-const ROOMS_DATA = Array(15)
-  .fill(1)
-  .map((index) => ({
-    id: Math.floor(Math.random() * 10) + 1,
-    cost: Math.floor(Math.random() * 15) + 10,
+export const ROOMS_DATA = Array(15)
+  .fill(null)
+  .map((_, index) => ({
+    id: index,
+    cost: Math.floor(Math.random() * 22) + 1,
     isReserved: false,
-    roomNumber: index,
   }));
 
-const USER_DATA = {
-  firstName: "Aastha",
-  lastName: "Kalra",
-  credits: Math.floor(Math.random() * 10) + 1,
+export const USER_DATA = {
+  lastName: 'Mr. Kalra',
+  credits: Math.floor(Math.random() * 25) + 5,
 };
-
-export const fetchUserDetails = () => {
-  return new Promise((resolve) => setInterval(() => resolve(USER_DATA), 1000));
-};
-export const fetchRoomDetails = () => {
-    return new Promise((resolve) => setInterval(() => resolve(ROOMS_DATA), 1000));
-  };
-  

3fb877572a2a3d27e8780b1bc932cf831a522f3e
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-11 20:36:51 +0530

    otp validation

---
+++ src/OTP/OTPValidation.jsx
@@ -0,0 +1,44 @@
+import React, {useState } from "react";
+
+const OTPValidation = () => {
+  const [otp, setOtp] = useState(Array(6).fill(null));
+  const [active, setActive] = useState(0);
+  const handleFocus = (index) => {
+    setActive(index);
+  };
+
+  const handleChange = (index, value) => {
+    if (value.match(/^[0-9]$/)) { // Allow only single digit numbers
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+
+      // Move focus to the next input if available
+      if (index < otp.length - 1) {
+        setActive(index + 1);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full px-8 py-4 ">
+      <div className="space-x-4 flex justify-center items-center my-5">
+        {otp.map((digit, i) => (
+          <input
+            type="text"
+            key={i}
+            maxLength={1}
+            value={digit}
+            onFocus={() => handleFocus(i)}
+            onChange={(e) => handleChange(i, e.target.value)}
+            className={`bg-transparent border-b-2  focus:outline-none w-12 h-12 ${
+              active === i ? "border-red-500" : "border-blue-950"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OTPValidation;

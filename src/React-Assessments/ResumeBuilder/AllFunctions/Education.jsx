10e03bdd265bdb7111d311bd2970d17556d2c9dd
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-15 20:10:09 +0530

    access values of education

--- src/React-Assessments/ResumeBuilder/AllFunctions/Education.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Education.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import InputControl from '../InputControl/InputControl'
-const Education = () => {
+const Education = ({value}) => {
   return (
     <div>
-        <InputControl label= "Title" placeholder="Enter title eg. B.Tech"/>
-        <InputControl label= "College/School Name" placeholder="Enter name of your college/school"/>
-        <InputControl label= "Start Date" placeholder="Enter start date of this education"/>
-        <InputControl label= "End Date" placeholder="Enter end date of this education"/>
+        <InputControl label= "Title" placeholder="Enter title eg. B.Tech"  defaultValue={value.title}/>
+        <InputControl label= "College/School Name" placeholder="Enter name of your college/school" defaultValue={value.college}/>
+        <InputControl label= "Start Date" placeholder="Enter start date of this education" defaultValue={value.startDate} />
+        <InputControl label= "End Date" placeholder="Enter end date of this education" defaultValue={value.endDate}/>
     </div>
   )
 }

2da564b21f76ee29f8a7178e996aade1374ad88e
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-16 19:44:13 +0530

    added handleChange

--- src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/BasicInfo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import InputControl from '../InputControl/InputControl'
 
-const BasicInfo = ({value}) => {
+const BasicInfo = ({value, handleChange}) => {
   return (
     <div>
-    <InputControl label="Name" placeholder="Enter your Name"  defaultValue={value.name}/>
+    <InputControl label="Name" placeholder="Enter your Name"  defaultValue={value.name}  onChange={(e)=>handleChange("name", e.target.value)}/>
     <InputControl label="Position" placeholder="Enter your Position eg. Frontend Developer" defaultValue={value.position}/>
     <InputControl label="Linkedin Link" placeholder="Enter your Linkedin profile link " defaultValue={value.linkedin}/>
     <InputControl label="Github Link" placeholder="Enter your Github profile link " defaultValue={value.github}/>

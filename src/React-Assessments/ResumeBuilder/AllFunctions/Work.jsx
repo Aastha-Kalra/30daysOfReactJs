3e71385280a98c018a7531fcc5fbca138cfaa5f1
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-17 21:35:15 +0530

    accessing handleChange in work

--- src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import InputControl from "../InputControl/InputControl";
-const Work = ({ value }) => {
+const Work = ({ value, handleChange }) => {
   return (
     <>
       <div>
@@ -9,26 +9,31 @@ const Work = ({ value }) => {
           label="Work "
           placeholder="Enter your experience (in years)"
           defaultValue={value.experience}
+          onchange={(e)=>handleChange('experience', e.target.value)}
         />
         <InputControl
           label="Position"
           placeholder="Enter your Position eg. Frontend Developer"
           defaultValue={value.position}
+          onchange={(e)=>handleChange('position', e.target.value)}
         />
         <InputControl
           label="Company Name"
           placeholder="Enter your company name eg. Microsoft"
           defaultValue={value.companyName}
+          onchange={(e)=>handleChange('companyName', e.target.value)}
         />
         <InputControl
           type="date"
           label="Start date"
           defaultValue={value.startDate}
+          onchange={(e)=>handleChange('startDate', e.target.value)}
         />
         <InputControl
           type="date"
           label="End date"
           defaultValue={value.endDate}
+          onchange={(e)=>handleChange('endDate', e.target.value)}
         />
         <div>
           <label className="text-2xl font-semibold pl-[68px] ">
@@ -37,10 +42,12 @@ const Work = ({ value }) => {
           <InputControl
             placeholder="Write about your experience"
             defaultValue={value.points ? value.points[0] : ""}
+            onchange={(e)=>handleChange('points[0]', e.target.value)}
           />
           <InputControl
             placeholder="Write about your experience"
             defaultValue={value.points ? value.points[1] : ""}
+            onchange={(e)=>handleChange('points[1]', e.target.value)}
           />
         </div>
       </div>

57e7d23c66e3818544abb2ddc246ac681e76d2ed
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-18 22:07:30 +0530

    accessing props to education

--- src/React-Assessments/ResumeBuilder/AllFunctions/Education.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Education.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import InputControl from '../InputControl/InputControl'
-const Education = ({value}) => {
+const Education = ({value, handleChange}) => {
   return (
     <div>
-        <InputControl label= "Title" placeholder="Enter title eg. B.Tech"  defaultValue={value.title}/>
+        <InputControl label= "Title" placeholder="Enter title eg. B.Tech"  defaultValue={value.title} onchange={(e)=>handleChange('title',e.target.value)}/>
         <InputControl label= "College/School Name" placeholder="Enter name of your college/school" defaultValue={value.college}/>
         <InputControl label= "Start Date" placeholder="Enter start date of this education" defaultValue={value.startDate} />
         <InputControl label= "End Date" placeholder="Enter end date of this education" defaultValue={value.endDate}/>

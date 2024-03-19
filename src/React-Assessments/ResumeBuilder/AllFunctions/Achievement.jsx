eea11c585719dc6f7109df1add1369a818d90ccc
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-19 20:17:19 +0530

    accesssing handleChange

--- src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import InputControl from '../InputControl/InputControl' 
-const Achievement = ({value}) => {
+import React from "react";
+import InputControl from "../InputControl/InputControl";
+const Achievement = ({ value, handleChange }) => {
   return (
     <div>
-        <InputControl label="Title" placeholder = "Enter the title eg. Reactjs Developer Ceritificate" defaultVzlue={value.title}/>
-        <InputControl label="Description" placeholder = "Line 1 "/>
-        <InputControl placeholder = "Line 2 " defaultVzlue={value.description}/>
+      <InputControl
+        label="Title"
+        placeholder="Enter the title eg. Reactjs Developer Ceritificate"
+        defaultVzlue={value.title}
+        onchange={(e) => handleChange("title", e.target.value)}
+      />
+      <InputControl label="Description" placeholder="Line 1 " />
+      <InputControl
+        placeholder="Line 2 "
+        defaultVzlue={value.description}
+        onchange={(e) => handleChange("description", e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Achievement
+export default Achievement;

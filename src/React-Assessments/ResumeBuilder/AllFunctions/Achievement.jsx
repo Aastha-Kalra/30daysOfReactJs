8fc66f23fee111ad07d87f3cd5127515daaa23db
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-19 20:16:41 +0530

    accessing values

--- src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Achievement.jsx
@@ -3,9 +3,9 @@ import InputControl from '../InputControl/InputControl'
 const Achievement = ({value}) => {
   return (
     <div>
-        <InputControl label="Title" placeholder = "Enter the title eg. Reactjs Developer Ceritificate"/>
+        <InputControl label="Title" placeholder = "Enter the title eg. Reactjs Developer Ceritificate" defaultVzlue={value.title}/>
         <InputControl label="Description" placeholder = "Line 1 "/>
-        <InputControl placeholder = "Line 2 "/>
+        <InputControl placeholder = "Line 2 " defaultVzlue={value.description}/>
     </div>
   )
 }

5b2593481a29f642d95aca737bac9b0753861590
Aastha kalra <aasthakalra2004@gmail.com>
2024-03-21 19:18:28 +0530

    handleChange in summary

--- src/React-Assessments/ResumeBuilder/AllFunctions/Summary.jsx
+++ src/React-Assessments/ResumeBuilder/AllFunctions/Summary.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import InputControl from '../InputControl/InputControl'
+import React from "react";
+import InputControl from "../InputControl/InputControl";
 
-const Summary = ({value}) => {
+const Summary = ({ value, handleChange }) => {
   return (
     <div>
-        <InputControl label="Summary" placeholder="Enter your Summary/Objective"/>
+      <InputControl
+        label="Summary"
+        placeholder="Enter your Summary/Objective"
+        defaultValue={value.summary}
+        onchange={(e) => handleChange("summary", e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;

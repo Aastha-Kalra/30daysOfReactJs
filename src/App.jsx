4c7951d268753f53f306338413f0cfa805b40705
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-01 22:22:35 +0530

    added sorting

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,7 @@ import LargestString from "./Javascript/LongestWord";
 import HashTag from "./Javascript/GenerateHash";
 import CountOccu from "./Javascript/CountOccu";
 import TriangleType from "./Javascript/TriangleType";
+import Sort from "./Javascript/Sort";
 function App() {
   return (
     <div className="bg-black min-h-screen w-full overflow-hidden text-green-500">
@@ -168,7 +169,8 @@ function App() {
       {/* <LargestString/> */}
       {/* <HashTag/> */}
       {/* <CountOccu/> */}
-      <TriangleType/>
+      {/* <TriangleType/> */}
+      <Sort/>
     </div>
   );
 }

360cf1f24679fbb83f15d55c9014e0cf3bb70e2f
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-06 22:50:54 +0530

    problem -10 : using reduce method SumOfNum

--- src/Javascript/SumOfNum.js
+++ src/Javascript/SumOfNum.js
@@ -1,12 +1,21 @@
 const SumOfNum = (num) => {
-    let sum=0;
-    let digits = num.toString();
 
-    for(let digit in digits){
-        sum+=parseInt(digit)
-    }
-    return sum
+    // let sum=0;
+    // let digits = num.toString();
+
+    // for(let digit in digits){
+    //     sum+=parseInt(digit)
+    // }
+    // return sum
+
+
+
+    // using reduce method
+    let arr = Array.from(String(num),Number)
+    return arr.reduce((acc,curr)=>acc+curr, 0)
 
 };
-console.log(SumOfNum(333));
+console.log(SumOfNum(334));
 export default SumOfNum;
+
+

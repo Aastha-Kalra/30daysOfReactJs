9d34f1ed3dac7415419c005c103b252ecf8a1f29
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-28 22:00:45 +0530

    showOutput

--- src/TextConvertor.jsx
+++ src/TextConvertor.jsx
@@ -3,55 +3,85 @@ import React, { useState } from "react";
 const TextConvertor = () => {
   const [input, setInput] = useState("");
   const [conversionType, setConversionType] = useState("UpperCase");
-  const [output, setOutput]=useState('')
+  const [output, setOutput] = useState("");
+  const [showOutput, setShowOutput] = useState(false);
   const Case = () => {
     if (conversionType === "UpperCase") {
-      let text = input.toUpperCase();
-      setOutput(text);
+      setOutput(input.toUpperCase());
+      setShowOutput(true);
     } else if (conversionType === "LowerCase") {
-      let text = input.toLowerCase();
-      setOutput(text);
+      setOutput(input.toLowerCase());
+      setShowOutput(true);
+    } else if (conversionType === "Encode Base64") {
+      setOutput(btoa(input));
+      setShowOutput(true);
+    } else if (conversionType === "Decode Base64") {
+      setOutput(atob(input));
+      setShowOutput(true);
     }
-    else if(conversionType==="Encode Base64"){
-        return setOutput(btoa(input))
-    }
-    else if(conversionType==="Decode Base64"){
-        return setOutput(atob(input))
-    }
-    return text;
   };
   return (
-    <div className="flex flex-col justify-center items-center gap-10 my-10 text-3xl">
-      <h1>TextConvertor</h1>
-      <textarea
-        name=""
-        id=""
-        cols="30"
-        rows="5"
-        className="bg-slate-300 focus:outline-none p-4 rounded-md font-semibold font-serif"
-        placeholder="Type your input here..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+    <div className="flex flex-col justify-start items-start gap-5 my-10 text-3xl px-10">
+      <h1 className="text-center w-full font-bold text-yellow-400">
+        TextConvertor
+      </h1>
 
-      <select
-        name=""
-        id=""
-        value={conversionType}
-        onChange={(e) => setConversionType(e.target.value)}
-        className="bg-slate-300 p-2 focus:outline-none"
-      >
-        <option value="UpperCase">UpperCase</option>
-        <option value="LowerCase">LowerCase</option>
-        <option value="Encode Base64">Encode Base64</option>
-        <option value="Decode Base64">Decode Base64</option>
-      </select>
+      {showOutput ? (
+        <div className=" flex flex-col w-full justify-center items-center">
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+            className="bg-yellow-50 focus:outline-none p-4 rounded-md font-semibold font-serif mt-10"
+          >
+            {output}
+          </textarea>
 
-      <button onClick={Case} className="bg-green-800 px-4 py-3 text-yellow-400 rounded-xl hover:scale-75 transition-all duration-500 ease-in">Submit</button>
+          <div className="flex gap-4 mt-10">
+            <button className="bg-green-800 px-10 py-3 text-yellow-400 rounded-xl hover:scale-95 transition-all duration-500 ease-in ">
+              Edit
+            </button>
+            <button className="bg-green-800 px-10 py-3 text-yellow-400 rounded-xl hover:scale-95 transition-all duration-500 ease-in ">
+              Convert More
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h2 className="">Text to be converted</h2>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+            className="bg-yellow-50 focus:outline-none p-4 rounded-md font-semibold font-serif"
+            placeholder="Type your input here..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
 
-      <div>
-        Output is : {output}
-      </div>
+          <select
+            name=""
+            id=""
+            value={conversionType}
+            onChange={(e) => setConversionType(e.target.value)}
+            className="bg-yellow-50 p-2 focus:outline-none"
+          >
+            <option value="UpperCase">UpperCase</option>
+            <option value="LowerCase">LowerCase</option>
+            <option value="Encode Base64">Encode Base64</option>
+            <option value="Decode Base64">Decode Base64</option>
+          </select>
+
+          <button
+            onClick={Case}
+            className="bg-green-800 px-4 py-3 text-yellow-400 rounded-xl hover:scale-95 transition-all duration-500 ease-in w-full"
+          >
+            Submit
+          </button>
+        </>
+      )}
     </div>
   );
 };

7da00373d22a6b2aee6acc80ea4bf32d33493d05
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-30 14:41:29 +0530

    factorial using forLoop

---
+++ src/Factorial.js
@@ -0,0 +1,11 @@
+const Factorial = (n) => {
+  //   using for loop
+  //   let fact = 1;
+  //   for (let i = 1; i <= n; i++) {
+  //     fact = fact * i;
+  //   }
+  //   return fact
+};
+
+console.log(Factorial(5));
+export default Factorial;

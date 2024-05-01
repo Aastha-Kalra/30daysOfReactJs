16dc9770f2358c839b5b622b329649c90dd8e083
Aastha kalra <aasthakalra2004@gmail.com>
2024-05-01 17:16:21 +0530

    objectEmptiness 2nd method

--- src/DSA/ObjectEmpty.js
+++ src/DSA/ObjectEmpty.js
@@ -1,10 +1,19 @@
 const ObjectEmpty = (obj) => {
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      return true;
-    }
-  }
-  return false
+// 1st method
+//   for (let key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       return true;
+//     }
+//   }
+//   return false
+
+
+// 2nd method
+
+return Object.keys(obj).length===0
+
+
 };
 console.log(ObjectEmpty({}));
+console.log(ObjectEmpty({name:"aastha"}));
 export default ObjectEmpty;

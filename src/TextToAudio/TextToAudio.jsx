c90866725547af4b35a8b27eb39daf58ff2bf14e
Aastha kalra <aasthakalra2004@gmail.com>
2024-05-06 16:22:03 +0530

    TextToAudio listener

---
+++ src/TextToAudio/TextToAudio.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from "react";
+import { WORDS_WITH_TIMINGS } from "./text.json";
+import song from "../assests/text.mp3";
+
+const TextToAudio = () => {
+  const [highlightedIndex, setHighlightedIndex] = useState(null);
+  const [visitedText, setVisitedText] = useState([]);
+  const audioRef = useRef();
+
+ 
+  return (
+    <div className="flex flex-col justify-center items-center px-44 bg-slate-900 text-xl gap-10 my-10 py-10">
+      <div>TextToAudio</div>
+      <p>
+        {WORDS_WITH_TIMINGS.map((w, index) => (
+          <span
+            key={index}
+            className={`${
+              index === highlightedIndex
+                ? "bg-gradient-to-r from-orange-400 to-green-500 bg-clip-text text-transparent"
+                : visitedText.includes(index)
+                ? "bg-gradient-to-r from-pink-600 to-pink-600 bg-clip-text text-transparent transition-all duration-500 ease-in-out"
+                : ""
+            }`}
+          >
+            {w.text}
+          </span>
+        ))}
+      </p>
+
+      <audio src={song} ref={audioRef} autoPlay controls></audio>
+    </div>
+  );
+};
+
+export default TextToAudio;

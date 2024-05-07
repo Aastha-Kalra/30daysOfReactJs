994732b5290f829ba970e14e8dfce71aa36f1c13
Aastha kalra <aasthakalra2004@gmail.com>
2024-05-07 16:58:07 +0530

    text.js for autocollapse

---
+++ src/AutoCollapse/text.js
@@ -0,0 +1,17 @@
+const data =  [
+    {
+      "h1": "What is devtools.tech",
+      "p1": "Devtools Tech is the leading platform for Frontend Engineers to enhance their skills,expand their knowledge, and prepare for technical interviews."
+    },
+
+    {
+      "h1": "Is it Free?",
+      "p1": "  Yes, the platform and YouTube both are completely free!"
+    },
+    {
+      "h1": "Take me already there",
+      "p1": "  Yes, the platform and YouTube both are completely free!"
+    }
+  ]
+
+export default data 

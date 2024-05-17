b646efcaeafaf4755e601ea9cdf0985b59841c0a
Aastha kalra <aasthakalra2004@gmail.com>
2024-05-17 20:38:44 +0530

    disabled after 10 digits

--- src/PasswordValidator/password.jsx
+++ src/PasswordValidator/password.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 
 const Password = () => {
-    const [password,setPassword] = useState('')
-    const [strength,setStrength] = useState(0)
-
-    useEffect(()=>{
-        if(password.length >=3){
-            setPassword(password)
-        }
-    },[password])
+  const [password, setPassword] = useState("");
+  const [strength, setStrength] = useState(0);
+  const passwordStrength = password.length;
+  useEffect(() => {
+    if (passwordStrength >= 3 && passwordStrength <= 10) {
+      setStrength(passwordStrength);
+    }
+  }, [password]);
   return (
     <div className="text-yellow-500">
-    <div>Password Validator</div>
-    <div>
-        <input type="password" value={password}  onChange={(e)=>setPassword(e.target.value)}/>
-        <p>Strength of your password (out of 10) is : {strength} </p>
+      <div>Password Validator</div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={passwordStrength >= 10}
+        />
+        <p>Strength of your password (out of 10) is: {strength}</p>
+      </div>
+      {password}
     </div>
-    {password}
-    </div>
-  )
+  );
 };
 
 export default Password;

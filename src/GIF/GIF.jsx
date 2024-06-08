8f3dbf5e0c424396191297b567123ca8c51ee12e
Aastha kalra <aasthakalra2004@gmail.com>
2024-06-08 19:38:51 +0530

    added searchResult for GIF

---
+++ src/GIF/GIF.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchResult from './SearchResult'
+
+const GIF = () => {
+  return (
+    <div className='text-white/50'>
+    <SearchResult/>
+    
+    </div>
+  )
+}
+
+export default GIF

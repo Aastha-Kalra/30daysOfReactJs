5c159fc29914b338991125a1e620a381cf43508b
Aastha kalra <aasthakalra2004@gmail.com>
2024-06-09 21:03:53 +0530

    added styling to SearchInput

--- src/GIF/SearchInput.jsx
+++ src/GIF/SearchInput.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const SearchInput = ({ HandleInput }) => {
   return (
-    <>
+    <div className="flex flex-col w-full justify-center items-center gap-4">
       <div>SearchInput</div>
-      <input type="text" onChange={(e) => HandleInput(e)} />
-    </>
+      <input type="text" className="bg-transparent text-green-600 w-full h-10 p-4 outline  outline-2  focus:outline-4 outline-yellow-300 rounded-md" onChange={(e) => HandleInput(e)} />
+    </div>
   );
 };
 

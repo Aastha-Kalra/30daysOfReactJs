825600a6541a6872232dcb2f7be9728d671292c0
Aastha kalra <aasthakalra2004@gmail.com>
2024-06-09 21:11:56 +0530

    updated props

--- src/GIF/SearchInput.jsx
+++ src/GIF/SearchInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const SearchInput = ({ HandleInput }) => {
+const SearchInput = ({ HandleSearch }) => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-4">
       <div>SearchInput</div>
-      <input type="text" className="bg-transparent text-green-600 w-full h-10 p-4 outline  outline-2  focus:outline-4 outline-yellow-300 rounded-md" onChange={(e) => HandleInput(e)} />
+      <input type="text" className="bg-transparent text-green-600 w-full h-10 p-4 outline  outline-2  focus:outline-4 outline-yellow-300 rounded-md" onChange={(e) => HandleSearch(e)} />
     </div>
   );
 };

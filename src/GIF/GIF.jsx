d54dfc915680afe5b6e2f16dca837de6bb86c19a
Aastha kalra <aasthakalra2004@gmail.com>
2024-06-09 21:03:18 +0530

    added filteredFunction

--- src/GIF/GIF.jsx
+++ src/GIF/GIF.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import SearchResult from './SearchResult'
-
+import React, { useState } from "react";
+import SearchResult from "./SearchResult";
+import SearchEmoji from "./SearchEmoji";
+import SearchInput from "./SearchInput";
 const GIF = () => {
+  const [filteredItem, setFilteredItem] = useState(SearchEmoji("", 50));
+  const HandleSearch = (e) => {
+    const filter = SearchEmoji(e.target.value);
+    setFilteredItem(filter);
+  };
   return (
-    <div className='text-white/50'>
-    <SearchResult/>
-    
+    <div className="text-white/50 flex flex-col gap-20 justify-center items-center my-5">
+      <SearchInput  HandleSearch={HandleSearch}/>
+      <SearchResult filteredItem={filteredItem} />
     </div>
-  )
-}
+  );
+};
 
-export default GIF
+export default GIF;
